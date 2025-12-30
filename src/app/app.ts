19f674619bb2e7832ea513385cc73c052aa409fb
powerrgav <powerrgav@gmail.com>
2025-12-30 15:50:30 -0500

    Begin work on portfolio website.
    Added basic header and subheader text. No animation on anything yet, still very barebones

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Main } from './main/main';
 
+
+/**
+ *  This is an early stage of the site's development
+ * I want to have the header and subheader text animate onto the page upon page load
+ * I want to have a picture of myself animate onto the screen after the header and subheader text
+ * I want to have some buttons that link to my github, one to linkedin, and potentially another that links to a blog
+ * I want to be able to scroll down and have the text and image animate away
+ * On scroll down, animate image of me in Barcelona or Paris
+ * Provides a detailed about me section, starting off with language skills (hence the picture)
+ */
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  imports: [RouterOutlet, Main],
+  template: `
+            <div class="fullscreenContainer">
+              <p class="headerText">Hi, I'm Gavin.</p>
+              <p class="subHeaderText">I am a fourth year Informatics student at Indiana University, graduating in May 2026.</p>
+              <p class="subHeaderText">This website hosts some of my work and a little bit about me!<p>
+            </div>`,
   styleUrl: './app.css'
 })
 export class App {
